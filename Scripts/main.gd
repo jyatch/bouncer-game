d87bf85eeb526d7fd6033b25a95a571024da0dd2
Cindy Vuong <146673760+cvyndy@users.noreply.github.com>
2026-09-19 15:59:31 -0400

    my cahnges

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Bouncer.play("Aakash")
+	$Bouncer.play("Jake")
 	$SpeechBubble.hide()
 	
 	pass # Replace with function body.
